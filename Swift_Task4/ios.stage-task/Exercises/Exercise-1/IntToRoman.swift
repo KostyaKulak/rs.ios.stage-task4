122150291d13363cfb19086ea671cd3a6aa8d72d
Kanstantsin Kulak <kkulak24@gmail.com>
2021-06-07 14:34:16 +0200

    feat: implement roman

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -1,8 +1,31 @@
 import Foundation
 
 public extension Int {
-    
+
     var roman: String? {
-        nil
+        if self < 1 || self > 3999 {
+            return nil
+        }
+        let yearsMap = [
+            1000: "M", 900: "CM", 500: "D", 400: "CD",
+            100: "C", 90: "XC", 50: "L", 40: "XL",
+            10: "X", 9: "IX", 5: "V", 4: "IV", 1: "I"
+        ]
+        let yearsKeys = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        var result = String()
+        var number = self
+        for key in yearsKeys {
+            if number >= key {
+                let quotient: Int = number / key
+                number = number % key
+                for _ in 1...quotient {
+                    result.append(yearsMap[key]!)
+                }
+            }
+            if number == 0 {
+                break
+            }
+        }
+        return result
     }
 }
